911f24702d080e538cc953cda4b1d5f24e4077b9
Watson Chua <watsonchua@gmail.com>
2024-06-17 21:57:15 +0800

    add bash script for generation

---
+++ inference/generate_mistral_wa.sh
@@ -0,0 +1,12 @@
+python generate_mistral.py --base_model_id="alpindale/Mistral-7B-v0.2-hf" \
+--lora_weights="data/mistral-7b-0.2-base-wa-friends-family" \
+--max_new_tokens=200 \
+--repetition_penalty=1.15 \
+--temperature=0.7 \
+--gradio=True \
+--gradio_port=8080 \
+--format_prompt=False \
+--truncate_prompt=True \
+--fastapi=False \
+--quantize=True \
+--custom_stop_tokens="<|eot_id|>"
